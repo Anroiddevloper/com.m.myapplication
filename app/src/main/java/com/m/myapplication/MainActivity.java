09981a9fd1dd0d1e7a3cf7a21d9e564d6e96a059
Payal <workholicdevs@gmail.com>
2019-11-02 17:37:36 +0530

    Test

--- app/src/main/java/com/m/myapplication/MainActivity.java
+++ app/src/main/java/com/m/myapplication/MainActivity.java
@@ -10,8 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*Share this comment*/
-        /*NOW IN THE SKY*/
-
     }
 }
