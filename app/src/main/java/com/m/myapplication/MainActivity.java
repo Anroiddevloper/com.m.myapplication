b7e95170b8a3c0e820579fe26f3483c678ef34af
Payal <workholicdevs@gmail.com>
2019-11-02 17:40:02 +0530

    Test

--- app/src/main/java/com/m/myapplication/MainActivity.java
+++ app/src/main/java/com/m/myapplication/MainActivity.java
@@ -10,11 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*Share this comment*/
-        /*NOW IN THE SKY*/
         
-        
-        MainActivity.this;
+
 
     }
 }
